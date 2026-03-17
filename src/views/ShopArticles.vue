bf50283ff0779136611916f3feb837bcbf5b3d71
josem <jjmondragon@vlacademy.com>
2026-03-17 16:53:03 -0600

    Latest modification for mobile

--- src/views/ShopArticles.vue
+++ src/views/ShopArticles.vue
@@ -312,7 +312,7 @@ text-shadow:
 
 @media (max-width: 768px) {
   .platform-filter {
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     gap: 8px;
     padding: 0 10px;
   }
